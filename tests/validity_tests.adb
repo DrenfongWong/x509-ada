15a425d113c2f5f91abf40ed49b9c93e5a15dca3
Reto Buerki <reet@codelabs.ch>
2012-10-30 10:28:29 +0100

    Extend validity test
    Load CA certificate with expired validity and assert that the Is_Valid
    function returns False.

--- tests/validity_tests.adb
+++ tests/validity_tests.adb
@@ -1,3 +1,4 @@
+with X509.Certs;
 with X509.Validity;
 
 package body Validity_Tests
@@ -10,9 +11,17 @@ is
 
    procedure Check_Is_Valid
    is
+      Cacert : Certs.Certificate_Type;
+      Val    : Validity.Validity_Type;
    begin
       Assert (Condition => not Validity.Is_Valid (V => Validity.Null_Validity),
               Message   => "Null validity valid");
+
+      Certs.Load (Filename => "data/ca_invalid.der",
+                  Cert     => Cacert);
+      Val := Certs.Get_Validity (Cert => Cacert);
+      Assert (Condition => not Validity.Is_Valid (V => Val),
+              Message   => "CA not invalid");
    end Check_Is_Valid;
 
    -------------------------------------------------------------------------
