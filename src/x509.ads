e30273da5e891dd079edf6010f15b466f4919ee1
Reto Buerki <reet@codelabs.ch>
2012-10-31 11:54:53 +0100

    Add Null_Byte_Array constant

--- src/x509.ads
+++ src/x509.ads
@@ -6,6 +6,8 @@ is
    type Byte is mod 2 ** 8;
    type Byte_Array is array (Positive range <>) of Byte;
 
+   Null_Byte_Array : constant Byte_Array (1 .. 0) := (others => 0);
+
    type TD_Handle is not null access all constr_TYPE_h.asn_TYPE_descriptor_t;
 
    Load_Error       : exception;
